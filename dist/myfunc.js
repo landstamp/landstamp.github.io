772199709affea89b0a7e94047ebbed1c8535146
sittinginatree <67747971+sittinginatree@users.noreply.github.com>
2020-11-12 16:52:10 +0000

    smoothscroll-v1

---
+++ dist/myfunc.js
@@ -0,0 +1,42 @@
+function smoothScroll (target,duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+    
+
+    function animation (currentTime){
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed,startPosition,distance,duration);
+        window.scrollTo(0,run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease (t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t*t + b;
+        t -= 2;
+        return c/2*(t*t*t + 2) + b;
+    } 
+
+    requestAnimationFrame(animation);
+}
+
+
+
+
+
+
+var arrow1 = document.querySelector('#arrow-1');
+
+arrow1.addEventListener('click', function() {
+    smoothScroll('#about-us',100);
+}); 
+
+var arrow2 = document.querySelector('#arrow-2');
+
+arrow2.addEventListener('click', function() {
+    smoothScroll('#our-services',100);
+}); 
